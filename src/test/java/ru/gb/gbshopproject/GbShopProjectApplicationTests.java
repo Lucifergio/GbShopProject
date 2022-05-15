1acc4890a9f1fcb4502f6da814cd4b9169cd63bc
aleksey <salnikovaleksey@yahoo.com>
2022-05-15 15:56:48 +0500

    init

---
+++ src/test/java/ru/gb/gbshopproject/GbShopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.gbshopproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbShopProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
